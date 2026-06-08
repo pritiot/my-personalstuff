4bac5c6d661b2ec92d5c4a64a22613aa364075e6
pritiot <castlexyzhelp@gmail.com>
2026-06-09 00:47:23 +0300

    Update securityinitScript.lua

--- securityinitScript.lua
+++ securityinitScript.lua
@@ -1,2 +1,15 @@
 -- nothing yet
-print("hello world")
+
+local oldNamecall
+oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    if self == game and method == "GetService" and (...) == "MicroProfilerService" then
+        return {
+            DumpToFileAsync = function() 
+                return "C:\\Users\\My name is Velocity Remastered and im gonna\\AppData\\Local\\Roblox\\logs\\67.html" 
+            end
+        }
+    end
+    
+    return oldNamecall(self, ...)
+end))
